1d43ddd06f3dd572225e9a7de222b4a8fd7b2936
Shaun Wassell <shaun.p.wassell@gmail.com>
2021-09-08 21:06:33 -0400

    05.05 - Implementing the stock ticker chart

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 import { getUserInfoRoute } from './getUserInfoRoute';
+import { getStockHistoryRoute } from './getStockHistoryRoute';
 
 export const routes = [
 	getUserInfoRoute,
+	getStockHistoryRoute,
 ];
--- src/server.js
+++ src/server.js
@@ -1,20 +1,25 @@
 import express from 'express';
-import { routes } from './routes';
 import { db } from './db';
-import { ALPHA_API_KEY } from './keys';
+import { routes } from './routes';
+import { tasks, scheduleTask } from './scheduled-tasks';
 
 const app = express();
 
 app.use(express.json());
 
+tasks.forEach(task => {
+    scheduleTask(task.handler, task.frequency, app);
+});
+
 routes.forEach(route => {
-	app[route.method](route.path, route.handler);
+    app[route.method](route.path, route.handler);
 });
 
 const start = async () => {
-	await db.connect('mongodb://localhost:27017');
-	await app.listen(8080);
-	console.log('Server is listening on port 8080')
+    await db.connect('mongodb://localhost:27017');
+    app.listen(8080,() => {
+        console.log('Server is listening on port 8080');
+    });
 }
 
 start();
