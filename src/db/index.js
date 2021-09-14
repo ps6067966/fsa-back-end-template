6a383925684f6fb29f212840543d751fa2dd70df
Shaun Wassell <shaun.p.wassell@gmail.com>
2021-09-14 09:02:14 -0400

    06.06 END - Implement the New Conversation Page

--- src/db/index.js
+++ src/db/index.js
@@ -1,3 +1,5 @@
 export { db } from './db';
 
+export { createConversation } from './createConversation';
+export { getAllUsers } from './getAllUsers';
 export { getUserConversations } from './getUserConversations';
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
+import { createConversationRoute } from './createConversationRoute';
+import { getAllUsersRoute } from './getAllUsersRoute';
 import { getUserConversationsRoute } from './getUserConversationsRoute';
 
 export { protectRoute } from './protectRoute';
 
 export const routes = [
+    createConversationRoute,
+    getAllUsersRoute,
     getUserConversationsRoute,
 ];
